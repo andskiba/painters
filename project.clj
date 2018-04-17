e455ff316ff678fe86f80f8d6d47f5219555a2cd
Andrzej Skiba <andskiba@gmail.com>
2018-04-18 00:00:06 +0200

    download all galleries from audrey kawasaki (https://www.audkawa.com)

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [http-kit "2.2.0"]]
+                 [http-kit "2.2.0"]
+                 [hickory "0.7.1"]]
   :main ^:skip-aot painters.core
   :target-path "target/%s"
   :profiles {:dev {:dependencies [[alembic "0.1.0"]]}
--- src/painters/core.clj
+++ src/painters/core.clj
@@ -1,7 +1,47 @@
 (ns painters.core
+  (:require [org.httpkit.client :as http]
+            [hickory.core :as h]
+            [hickory.select :as s]
+            [clojure.java.io :as io]
+            [clojure.string :as str])
   (:gen-class))
+
+(defn get-page [url]
+  (-> @(http/get url) :body h/parse h/as-hickory))
+
+(defn galleries []
+  (let [page (get-page "https://www.audkawa.com/")]
+    (into #{}
+          (map #(get-in % [:attrs :href])
+               (s/select (s/child (s/class "gallery-collection")
+                                  (s/tag :a))
+                         page)))))
+
+(defn gallery-images [gallery]
+  (let [page (get-page (str "https://www.audkawa.com" gallery))]
+    (map #(get-in % [:attrs :data-src])
+         (s/select (s/child (s/class "slideshow")
+                            (s/class "slide")
+                            (s/tag :img))
+                   page))))
+
+(defn write-file [file-path bytes]
+  (with-open [out (io/output-stream (io/file file-path))]
+    (.write out bytes)))
+
+(defn url-to-file-name [url]
+  (-> (str/split url #"\/") reverse first))
+
+(defn download-image [target-dir url]
+  (let [file-name (url-to-file-name url)]
+    (write-file (str target-dir file-name)
+                (.bytes (:body @(http/get url))))))
+
+(defn download-audrey []
+  (doseq [img (mapcat gallery-images (galleries))]
+    (download-image "/tmp/painters/" img)))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (download-audrey))
